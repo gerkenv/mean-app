ea21a61ec286d7b1338ce83646cc8d7fe6472335
gerkenv <gerkenv@gmail.com>
2018-09-24 01:02:52 +0200

    db remote address was to my deployment at `mlab.com`;

--- config/database.js
+++ config/database.js
@@ -1,4 +1,5 @@
 module.exports = {
-    database: 'mongodb://localhost:27017/meanauth',
+    // database: 'mongodb://localhost:27017/meanauth',
+    database: 'mongodb://john2:jm123456@ds111430.mlab.com:11430/mean-app',
     secret: 'yoursecret'
 };
